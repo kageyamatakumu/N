f27bc64f2eae10e8e012776b6afe88114680f6d8
takumu <takumu155695@gmail.com>
2022-03-29 11:53:42 +0900

    Add: タイトルを動的に表示

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
+  # タイトルを動的に
+  def page_title(page_title = '')
+    base_title = 'Nemo'
+
+    page_title.empty? ? base_title : page_title + ' | ' + base_title
+  end
+
+  # 管理画面のタイトルを動的に
+  def page_admin_title(page_title = '')
+    base_title = 'Nemo管理画面'
+
+    page_title.empty? ? base_title : page_title + ' | ' + base_title
+  end
 end
