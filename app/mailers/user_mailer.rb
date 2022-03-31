8ffb411f4fe894567b77f2576060791cb8296e3b
takumu <takumu155695@gmail.com>
2022-03-31 17:25:55 +0900

    Add: ActionMailerが本番環境で動くように

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,4 +1,7 @@
 class UserMailer < ApplicationMailer
+
+  default from: 'nemo.fashionsite2022@gmail.com'
+
   def reset_password_email(user)
     @user = User.find user.id
     @url  = edit_password_reset_url(@user.reset_password_token)
