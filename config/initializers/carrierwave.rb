8de5349388b2e250bf11e140ff33788559f662ff
takumu <takumu155695@gmail.com>
2022-04-04 13:15:11 +0900

    Add: S3で画像を保存できるように設定

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -6,7 +6,6 @@ CarrierWave.configure do |config|
     config.storage :fog
     config.fog_provider = 'fog/aws'
     config.fog_directory  = 'nemo.brands' # 作成したバケット名を記述
-    config.fog_public = false
     config.fog_credentials = {
       provider: 'AWS',
       aws_access_key_id: Settings.AWS_ACCESS_KEY_ID, # 環境変数
