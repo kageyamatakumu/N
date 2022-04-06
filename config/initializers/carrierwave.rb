1dca58dbe59d5a8f1d9d5c5ba6cd7599b8028cad
takumu <takumu155695@gmail.com>
2022-04-06 15:12:33 +0900

    Add: aws_s3を修正

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -5,7 +5,7 @@ require 'carrierwave/storage/fog'
 CarrierWave.configure do |config|
     config.storage :fog
     config.fog_provider = 'fog/aws'
-    config.fog_directory  = 'daison'
+    config.fog_directory  = 'takumu'
     # config.fog_public = false
     config.fog_credentials = {
       provider: 'AWS',
