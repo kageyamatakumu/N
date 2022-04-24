6627cb7b09fee9ec99b9d88bab91b6d8094f89a9
takumu <takumu155695@gmail.com>
2022-04-24 09:16:18 +0900

    Add: Ajax処理に移行

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,13 +1,15 @@
 class LikesController < ApplicationController
   def create
-    item = Item.find(params[:item_id])
-    current_user.like(item)
-    redirect_back(fallback_location: brand_item_path(item.brand, item))
+    @item = Item.find(params[:item_id])
+    current_user.like(@item)
+    # ajax(非同期処理に移行)
+    # redirect_back(fallback_location: brand_item_path(item.brand, item))
   end
 
   def destroy
-    item = current_user.likes.find(params[:id]).item
-    current_user.unlike(item)
-    redirect_back(fallback_location: brand_item_path(item.brand, item))
+    @item = current_user.likes.find(params[:id]).item
+    current_user.unlike(@item)
+    # ajax(非同期処理に移行)
+    # redirect_back(fallback_location: brand_item_path(item.brand, item))
   end
 end
